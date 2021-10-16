7af49488f6fcad5f0820413cb2a64ce2049f29a6
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-10-16 10:52:07 -0400

    Add files via upload

--- src/main/java/SearchByPlayerAttributes.java
+++ src/main/java/SearchByPlayerAttributes.java
@@ -1,3 +1,33 @@
-class SearchByPlayerAttributes {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class SearchByPlayerAttributes{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(searchPlayer(90, 80, 90)); // just a sample
+    }
+    public static String searchPlayer(int val1, int val2, int val3) throws IOException {
+        //we can add more vals eventually
+        String resultRow = null;
+        BufferedReader br = new BufferedReader(new FileReader("./dataset(s)/players_20.csv"));
+        String l;
+        int iteration = 0;
+        while ((l = br.readLine()) != null)  // parses until we reach the end of line
+        {
+            if(iteration == 0) {
+                iteration++;
+                continue;
+            }
+            String[] val = l.split(",");
+            if(Integer.parseInt(val[column1]) > val1 && Integer.parseInt(val[column2]) > val1 &&
+                    Integer.parseInt(val[column3]) > val1 ) {
+                resultRow = l;
+                break;
+            }
+        }
+        br.close();
+        return resultRow;
+    }
 }
