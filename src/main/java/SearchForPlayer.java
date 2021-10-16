17a7795e5a69346ab2012345a9d95a206c59ef22
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-10-16 10:37:37 -0400

    Add files via upload
    Added search by player functionality after getting input from user

--- src/main/java/SearchForPlayer.java
+++ src/main/java/SearchForPlayer.java
@@ -1,34 +1,28 @@
-import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.File;
+import java.util.Scanner;
 
-class SearchForPlayer {
+public class SearchForPlayer{
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the player you want to search for:");
+        System.out.println("Enter the player you want to search for:"); //gets the query from client
         String inp = sc.nextLine();
-        System.out.println("Searching for " + inp);
-        BufferedReader b = null;
-        String line = "";
-        String cvsSplitBy = ",";
-        //System.out.println(new File("players_20.csv").getAbsoluteFile());
-        String path = "";
-        b = new BufferedReader(new FileReader("players_20.csv"));
-        while ((line = b.readLine()) != null) {
-            // using comma as separator
-            String[] lineArray = line.split(cvsSplitBy);
-            //Check the value from array
-            if(lineArray[2] == inp) {
-                System.out.println(lineArray);
-                return;
-            }
-            break;
-        }
+        System.out.println(searchPlayer(2, inp));
     }
-
-
-
+    public static String searchPlayer(int ColumnIndex, String player) throws IOException {
+        String resultRow = null;
+        BufferedReader br = new BufferedReader(new FileReader("./dataset(s)/players_20.csv"));
+        String l;
+        while ((l = br.readLine()) != null)  // parses until we reach the end of line
+        {
+            String[] val = l.split(",");
+            if(val[ColumnIndex].equals(player)) {
+                resultRow = l;
+                break;
+            }
+        }
+        br.close();
+        return resultRow;
+    }
 }
