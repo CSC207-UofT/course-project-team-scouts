1bc5e2e16db0f5c757e8dca4b2bcf17e1d5446b5
m-parvaneh <mattparvaneh@outlook.com>
2021-10-16 18:17:50 -0400

    Modification to PlayersPresenter

--- src/main/java/PlayersPresenter.java
+++ src/main/java/PlayersPresenter.java
@@ -1,5 +1,5 @@
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class PlayersPresenter implements PresentData<Player> {
     @Override
@@ -8,11 +8,11 @@ public class PlayersPresenter implements PresentData<Player> {
         if (playerList.size() > 0) {
             // Get the first map for the first player in the list, so we
             // can model the header for the table on this.
-            Map<String, Integer> attributes = playerList.get(0).getSkills();
+            HashMap<String, Integer> attributes = playerList.get(0).getSkills();
 
             // Use all keys in HashMap as the column names for the table.
             StringBuilder header = new StringBuilder();
-            header.append(String.format("%20s", "Name")).append("|");
+            header.append(String.format("%40s", "Name")).append("|");
             header.append(String.format("%20s", "Age")).append("|");
 
             for (String skill : attributes.keySet()) {
@@ -27,7 +27,7 @@ public class PlayersPresenter implements PresentData<Player> {
             // values for their skill attributes.
             StringBuilder playerRow = new StringBuilder();
 
-            playerRow.append(String.format("%20s", p.getName())).append("|");
+            playerRow.append(String.format("%40s", p.getName())).append("|");
             playerRow.append(String.format("%20s", p.getAge())).append("|");
 
             for (Integer skillValue : p.getSkills().values()) {
