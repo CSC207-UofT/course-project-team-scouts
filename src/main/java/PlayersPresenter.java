aaa3281e663bcbc35a0bc7fb724a075f3eaa7e24
m-parvaneh <mattparvaneh@outlook.com>
2021-11-01 14:57:45 -0400

    Refactoring PlayersPresenter

--- src/main/java/PlayersPresenter.java
+++ src/main/java/PlayersPresenter.java
@@ -11,10 +11,44 @@ public class PlayersPresenter implements PresentData<Player> {
     @Override
     public void outputResults(List<Player> playerList) {
         // Output the header of the table.
-        if (playerList.size() > 0) {
+        printHeader(playerList);
+
+        // Loop through all players in the list to get their skill values.
+        printRows(playerList);
+    }
+
+    /**
+     * Prints a nicely-formatted row containing all the relevant details for
+     * each player returned by the search.
+     * @param players a list of Player entities that were yielded by the
+     */
+    private void printRows(List<Player> players) {
+        for (Player p: players) {
+            // Build a row in the table corresponding to a given player and the
+            // values for their skill attributes.
+            StringBuilder playerRow = new StringBuilder();
+
+            playerRow.append(String.format("%40s", p.getName())).append("|");
+            playerRow.append(String.format("%20s", p.getAge())).append("|");
+
+            for (Integer skillValue : p.getSkills().values()) {
+                playerRow.append(String.format("%20d", skillValue)).append("|");
+            }
+            System.out.println(playerRow);
+        }
+    }
+
+    /**
+     * Prints a header for the tabular output of all players returned by the search,
+     * which contains the names of all the attributes of the players being listed
+     * in each column.
+     * @param players
+     */
+    private void printHeader(List<Player> players) {
+        if (players.size() > 0) {
             // Get the first map for the first player in the list, so we
             // can model the header for the table on this.
-            HashMap<String, Integer> attributes = playerList.get(0).getSkills();
+            HashMap<String, Integer> attributes = players.get(0).getSkills();
 
             // Use all keys in HashMap as the column names for the table.
             StringBuilder header = new StringBuilder();
@@ -27,21 +61,6 @@ public class PlayersPresenter implements PresentData<Player> {
             System.out.println(header);
         } else {
             System.out.println("No players found.");
-        }
-
-        // Loop through all players in the list to get their skill values.
-        for (Player p: playerList) {
-            // Build a row in the table corresponding to a given player and the
-            // values for their skill attributes.
-            StringBuilder playerRow = new StringBuilder();
-
-            playerRow.append(String.format("%40s", p.getName())).append("|");
-            playerRow.append(String.format("%20s", p.getAge())).append("|");
-
-            for (Integer skillValue : p.getSkills().values()) {
-                playerRow.append(String.format("%20d", skillValue)).append("|");
-            }
-            System.out.println(playerRow);
         }
     }
 }
