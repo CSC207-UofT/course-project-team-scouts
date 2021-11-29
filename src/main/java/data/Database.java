4b0dd07f5570e0d0e72221dcdfe013f4ae1501ff
Daniel Lazaro <d.lazaro@mail.utoronto.ca>
2021-11-29 16:53:10 -0500

    Make Database non-abstract, generic

--- src/main/java/data/Database.java
+++ src/main/java/data/Database.java
@@ -1,12 +1,44 @@
 package data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Database superclass with subclasses that store team
  * and player databases.
  */
-abstract class Database {
-    public abstract void addEntity();
-    public abstract Object createEntity(String name, List<Object> objects);
+class Database<T> {
+    private List<T> entityList;
+
+    public Database() {
+        entityList = new ArrayList<>();
+    }
+
+    /**
+     * Adds new entity to the database
+     *
+     * @param entity object to be added to database
+     */
+    public void addEntity(T entity) {
+        entityList.add(entity);
+    }
+    
+    /** 
+     * Sets the list of entities in the database to
+     * the given list
+     * 
+     * @param entities list of entity objects
+     */
+    public void setEntities(List<T> entities) {
+        entityList = entities;
+    }
+
+    /**
+     * Returns a list of enities in the database
+     *
+     * @return list of entity objects
+     */
+    public List<T> getEntities() {
+        return entityList;
+    }
 }
