da5d5eb88bbb933729fb63dc292742b4907b9e0a
Daniel Lazaro <d.lazaro@mail.utoronto.ca>
2021-11-29 22:44:14 -0500

    Tobey's changes to CommandLine

--- src/main/java/ui/CommandLine.java
+++ src/main/java/ui/CommandLine.java
@@ -4,10 +4,12 @@ import data.PlayerDatabase;
 import data.TeamDatabase;
 import entities.User;
 import entities.UserList;
+import data.UserDatabase;
 import io.InputBuilder;
 import io.InputData;
 import io.LoginController;
 import io.LoginUseCase;
+import io.InputLogin;
 import services.CSVAdapter;
 
 import java.io.BufferedReader;
@@ -27,14 +29,38 @@ public class CommandLine {
     public static void main(String[] args) throws IOException {
         // Initialize adapter
         CSVAdapter adapter = new CSVAdapter();
-        UserList users = new UserList();
+
+        // Initialize databases
+        UserDatabase users = new UserDatabase();
         PlayerDatabase playerDatabase = new PlayerDatabase();
         TeamDatabase teamDatabase = new TeamDatabase();
-        users.add(new User("John", "123")); //adds a pre-defined user to list of users database
+
+        // Run login process
         LoginUseCase useCase = new LoginUseCase(users); //use case
         LoginController controller = new LoginController(useCase); //controller
         adapter.processFile("dataset(s)/players_20.csv", playerDatabase, teamDatabase);
         runPrompts(controller);
+    }
+
+    public static void runLoginPrompt() {
+        InputLogin inputLogin = new InputLogin();
+        try (inputLogin.run()) {
+
+        } catch (IOException e) {
+
+        }
+        controller.runLogin(username, password);
+
+        System.out.print("Would you like to search for players based on their name or attributes?" +
+                " (Please input 'name' or 'attributes'): ");
+        String s = reader.readLine();
+
+        try {
+            InputData inputClass = InputBuilder.getInputType(s);
+            inputClass.run();
+        } catch (NullPointerException e) {
+            System.out.println("Invalid Response");
+        }
     }
 
     /**
@@ -45,25 +71,7 @@ public class CommandLine {
      * @throws IOException method prone to IOException when taking input
      */
     private static void runPrompts(LoginController controller) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Username: ");
-        String username = reader.readLine();
-        System.out.println("Password: ");
-        String password = reader.readLine();
-        controller.runLogin(username, password);
-
-        System.out.print("Would you like to search for players based on their name or attributes?" +
-                " (Please input 'name' or 'attributes'): ");
-        String s = reader.readLine();
-
-        try {
-            InputData inputClass = InputBuilder.getInputType(s);
-            inputClass.run();
-
-        } catch (NullPointerException e) {
-            System.out.println("Invalid Response");
-        }
     }
 
     /**
