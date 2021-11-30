7c8374038a1c33c144dc14696f474b9874564077
Daniel Lazaro <d.lazaro@mail.utoronto.ca>
2021-11-29 19:50:47 -0500

    Update Database classes and Player

--- src/main/java/data/PlayerDatabase.java
+++ src/main/java/data/PlayerDatabase.java
@@ -1,48 +1,18 @@
 package data;
 
 import entities.Player;
-import entities.PlayerFactory;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 /**
  * Responsible for storing Player Data as a list of player entities
  */
-public class PlayerDatabase extends Database {
-    private List<Player> players = new ArrayList<>();
+public class PlayerDatabase extends Database<Player> {    
+    // This method is unnecessary - CSVAdapter calls PlayerFactory, which
+    // calls the constructor of Player
+    // public Player createPlayer(String name, int age, double height, double weight,
+    //                            String team, int rating, double value, String position,
+    //                            HashMap<String, Integer> skills) {
+    //     return PlayerFactory.makePlayer(name, age, height, weight, team, rating, value, position, skills);
+    // }
 
-    /**
-     * Adds new player to Player database
-     *
-     * @param player player object to be added to database
-     */
-    public Player createPlayer(String name, int age, double height, double weight,
-                               String team, int rating, double value, String position,
-                               HashMap<String, Integer> skills) {
-        return PlayerFactory.makePlayer(name, age, height, weight, team, rating, value, position, skills);
-    }
-    @Override
-    public void addEntity(Player player) {
-        players.add(player);
-    }
-
-    /**
-     * Setter for Players
-     *
-     * @param playerList list of player entities
-     */
-    public void setPlayers(List<Player> playerList) {
-        players = playerList;
-    }
-
-    /**
-     * Getter for Players
-     *
-     * @return Players list of player entities
-     */
-    public List<Player> getPlayers() {
-        return players;
-    }
+    // TODO: Add functionality to this class that makes it unique from Database
 }
--- src/main/java/data/TeamDatabase.java
+++ src/main/java/data/TeamDatabase.java
@@ -3,53 +3,34 @@ package data;
 import entities.Player;
 import entities.Team;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Responsible for storing Team Data as a list of Team entities
  */
-public class TeamDatabase extends Database {
-    private List<Team> teams = new ArrayList<>();
-
-    /**
-     * Adds new team to Team database
-     *
-     * @param name    team name as string
-     * @param players arraylist of players on team
+public class TeamDatabase extends Database<Team> {
+    // Unnecessary! (I think)
+    // /**
+    //  * Adds new team to Team database
+    //  *
+    //  * @param name    team name as string
+    //  * @param players arraylist of players on team
+    //  */
+    // public Team createTeam(String name, List<Player> players) {
+    //     return new Team(name, players);
+    // }
+    
+    /** 
+     * Updates the given team's roster to include the given
+     * Player object
+     * 
+     * @param teamName String representing team name
+     * @param player   Player object to be added to the team
      */
-    public Team createTeam(String name, List<Player> players) {
-        return new Team(name, players);
-    }
-
-    @Override
-    public void addEntity(Team team) {
-        teams.add(team);
-    }
-
-    public void updateRoster(String t_name, Player player) {
-        for (Team t : teams) {
-            if (t.getTeamName().equals(t_name)) {
+    public void updateRoster(String teamName, Player player) {
+        for (Team t : entityList) {
+            if (t.getTeamName().equals(teamName)) {
                 t.addPlayer(player);
+                player.updateTeam(teamName);
             }
         }
-    }
-
-    /**
-     * Setter for Teams
-     *
-     * @param teamList list of  entities
-     */
-    public void setTeams(List<Team> teamList) {
-        teams = teamList;
-    }
-
-    /**
-     * Getter for Teams
-     *
-     * @return list of Team entities
-     */
-    public List<Team> getTeams() {
-        return teams;
     }
 }
