889549ef5d4005d59d42ffd5a20f25d5154e046b
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-12-04 10:34:11 -0500

    Add files via upload

--- src/main/java/entities/Midfielder.java
+++ src/main/java/entities/Midfielder.java
@@ -1,8 +1,23 @@
 package entities;
 
 import java.util.HashMap;
-
+/**
+ * A midfielder with attributes and information.
+ */
 public class Midfielder extends Player {
+    /**
+     * A new midfielder with attributes and information.
+     *
+     * @param name the name
+     * @param age the age
+     * @param height the height
+     * @param weight the weight
+     * @param team the team
+     * @param rating the rating
+     * @param value the value
+     * @param position the position
+     * @param skills the skills
+     */
     public Midfielder(String name, int age, double height, double weight, String team, int rating,
                       double value, String position, HashMap<String, Integer> skills) {
         super(name, age, height, weight, team, rating, value, position, skills);
--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -2,21 +2,50 @@ package entities;
 
 import java.util.HashMap;
 
-/*
- * Class dealing w/ a player entity
- * */
+/**
+ * A player with his attributes and information.
+ */
 public class Player {
+    /**
+     * The name.
+     */
     private String name;
+    /**
+     * The age.
+     */
     private int age;
+    /**
+     * The height.
+     */
     private final double height;
+    /**
+     * The weight.
+     */
     private final double weight;
+    /**
+     * The team.
+     */
     private final String team;
+    /**
+     * The rating.
+     */
     private final int rating;
+    /**
+     * The value.
+     */
     private final double value;
+    /**
+     * The position.
+     */
     private final String position;
+    /**
+     * Hashmap of skill name to skill value
+     */
     private final HashMap<String, Integer> skills;
 
-    // Default Constructor
+    /**
+     * Default constructor
+     */
     public Player() {
         this.name = "N/A";
         this.age = 0;
@@ -28,7 +57,19 @@ public class Player {
         this.position = null;
         this.skills = new HashMap<>();
     }
-
+    /**
+     * A new player with attributes and information.
+     *
+     * @param name the name
+     * @param age the age
+     * @param height the height
+     * @param weight the weight
+     * @param team the team
+     * @param rating the rating
+     * @param value the value
+     * @param position the position
+     * @param skills the skills
+     */
     public Player(String name, int age, double height, double weight, String team,
                   int rating, double value, String position,
                   HashMap<String, Integer> skills) {
@@ -42,67 +83,116 @@ public class Player {
         this.position = position;
         this.skills = skills;
     }
-
+    /**
+     * Return the hashmap of this player's skills.
+     */
     public HashMap<String, Integer> getSkills() {
         return this.skills;
     }
 
+    /**
+     * Return the name of the player.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Return the age of this player.
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Return the height of this player.
+     */
     public double getHeight() {
         return height;
     }
 
+    /**
+     * Return the weight of this player.
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Return the rating of this player.
+     */
     public int getRating() {
         return rating;
     }
 
+    /**
+     * Return the value of this player.
+     */
     public double getValue() {
         return value;
     }
 
+    /**
+     * Return the strength of this player.
+     */
     public int getStrength() {
         return skills.get("strength");
     }
 
+    /**
+     * Return the stamina of this player.
+     */
     public int getStamina() {
         return skills.get("stamina");
     }
 
-    public void setName(String pos) {
-        name = pos;
+    /**
+     * Set the name of the player.
+     * @param namePlayer the name.
+     */
+    public void setName(String namePlayer) {
+        name = namePlayer;
     }
 
-    public void setAge(int x) {
-        age = x;
+    /**
+     * Set the age of the player.
+     * @param agePlayer the age.
+     */
+    public void setAge(int agePlayer) {
+        age = agePlayer;
     }
 
+    /**
+     * Display player's name.
+     */
     public void displayName() {
         System.out.println("This player's position is " + getName());
     }
 
+    /**
+     * Display player's age.
+     */
     public void displayAge() {
         System.out.println("This player's age is " + getAge());
     }
 
+    /**
+     * Return player's team.
+     */
     public String getTeam() {
         return team;
     }
 
+    /**
+     * Return player's position.
+     */
     public String getPosition() {
         return position;
     }
 
+    /**
+     * Display player's position.
+     */
     public void displayPos() {
         System.out.println("This player's position is " + getPosition());
     }
