e18b0d2b2f9a3238f28e3b03268d2462de665c73
Daniel Lazaro <d.lazaro@mail.utoronto.ca>
2021-12-03 21:57:08 -0500

    Add loadDatabases and saveDatabases methods to ReadWriter

--- src/main/java/io/ReadWriter.java
+++ src/main/java/io/ReadWriter.java
@@ -1,6 +1,9 @@
 package io;
 
 import data.Database;
+import data.PlayerDatabase;
+import data.TeamDatabase;
+import data.UserDatabase;
 
 import java.io.*;
 
@@ -12,7 +15,7 @@ public class ReadWriter {
      * @param database stores the list of entities to be serialized
      * @throws IOException can throw input/output exception
      */
-    public void saveToFile(String filePath, Database<?> database) throws IOException {
+    private static void saveToFile(String filePath, Database<?> database) throws IOException {
         OutputStream file = new FileOutputStream(filePath);
         OutputStream buffer = new BufferedOutputStream(file);
         ObjectOutput output = new ObjectOutputStream(buffer);
@@ -29,7 +32,7 @@ public class ReadWriter {
      * @return list of entities
      * @throws IOException can throw input/output exception
      */
-    public Database<?> readFromFile(String filePath) throws IOException, ClassNotFoundException {
+    private static Database<?> readFromFile(String filePath) throws IOException, ClassNotFoundException {
         InputStream file = new FileInputStream(filePath);
         InputStream buffer = new BufferedInputStream(file);
         ObjectInput input = new ObjectInputStream(buffer);
@@ -38,5 +41,33 @@ public class ReadWriter {
         Database<?> database = (Database<?>) input.readObject();
         input.close();
         return database;
+    }
+
+    public static Database<?>[] loadDatabases() {
+        try {
+            UserDatabase userDatabase = (UserDatabase) readFromFile("dataset(s)/users.ser");
+            PlayerDatabase playerDatabase = (PlayerDatabase) readFromFile("dataset(s)/players.ser");
+            TeamDatabase teamDatabase = (TeamDatabase) readFromFile("dataset(s)/teams.ser");
+            return new Database<?>[]{userDatabase, playerDatabase, teamDatabase};
+        } catch (IOException | ClassNotFoundException e) {
+            // If we are in this catch, then one of the files above is missing
+            // We must rebuild the databases from scratch
+            UserDatabase userDatabase = new UserDatabase();
+            PlayerDatabase playerDatabase = new PlayerDatabase();
+            TeamDatabase teamDatabase = new TeamDatabase();
+            return new Database<?>[]{userDatabase, playerDatabase, teamDatabase};
+        }
+    }
+
+    public static boolean saveDatabases(UserDatabase users, PlayerDatabase players, TeamDatabase teams) {
+        try {
+            saveToFile("dataset(s)/users.ser", users);
+            saveToFile("dataset(s)/players.ser", players);
+            saveToFile("dataset(s)/teams.ser", teams);
+            return true;
+        } catch (IOException e) {
+            // Some error has occurred! We did not successfully save!
+            return false;
+        }
     }
 }
