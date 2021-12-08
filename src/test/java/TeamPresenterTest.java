8ae14fb361e827bdcbd4a38fb109bf90cd09f3d8
m-parvaneh <mattparvaneh@outlook.com>
2021-12-08 00:32:17 -0500

    Minor modifications to presenters and presenter tests.

--- src/test/java/TeamPresenterTest.java
+++ src/test/java/TeamPresenterTest.java
@@ -23,6 +23,7 @@ public class TeamPresenterTest {
 
     https://www.baeldung.com/java-testing-system-out-println
      */
+    private HashMap<String, Integer> defaultSkills;
     private TeamsPresenter presenter;
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
@@ -35,6 +36,22 @@ public class TeamPresenterTest {
         Database<Team> database;
         System.setOut(new PrintStream(outputStreamCaptor));
 
+        defaultSkills = new HashMap<>();
+        String[] skillTypes = {"crossing" ,"finishing",
+                "heading accuracy","short passing",
+                "volleys","dribbling" ,"curve",
+                "fk accuracy", "long passing",
+                "ball control", "acceleration" , "sprint speed",
+                "agility", "reactions", "balance",
+                "shot_power", "jumping", "stamina", "strength",
+                "long_shots", "aggression", "interceptions",
+                "positioning", "vision", "penalties",
+                "composure","marking","standing tackle",
+                "sliding tackle","goalkeeping diving","goalkeeping handling",
+                "goalkeeping kicking","goalkeeping positioning","goalkeeping reflexes"};
+        for (int i = 0; i <= skillTypes.length - 1; i = i + 1){
+            defaultSkills.put(skillTypes[i], 10);
+        }
     }
 
     @After
@@ -56,20 +73,26 @@ public class TeamPresenterTest {
         assertEquals(
                 "================================================\n" + "No matching teams found.\n", outputStreamCaptor.toString());
     }
-    /*
-    @Test
-    public void testOutputResultsWithTeam() {
-        Team t = new Team();
-        String teamName = "Real Madrid";
-        data.TeamDatabase database = new data.TeamDatabase();
-        SearchByName<Team> searchByName = new SearchByName<>();
-        this.searchResults = searchByName.search(database, teamName);
-        TeamsPresenter tPresenter = new TeamsPresenter();
-        tPresenter.outputResults(searchResults);
-        assertEquals(
-                "=============================================\n" + "No matching teams found.\n", outputStreamCaptor.toString());
-    }
-
-     */
+//    @Test
+//    public void testOutputResultsWithTeam() {
+//        List<Team> teams = new ArrayList<>();
+//        List<Player> players = new ArrayList<>();
+//
+//        players.add(new Player("Cristiano Ronaldo", 35, 181, 75, "Real Madrid",
+//                92, 90000000, "LW", defaultSkills));
+//        players.add(new Player("Lionel Messi", 33, 175, 70, "Real Madrid",
+//                94, 90000000, "CF", defaultSkills));
+//        teams.add(new Team("Real Madrid", players));
+//
+//        presenter.outputResults(teams);
+//        assertEquals(
+//                "================================================\n"+
+//                        "Team id: 0\n"+
+//                        "Team name: Real Madrid\n"+
+//                        "Overall Rating: 93\n"+
+//                        "Net worth: 180000000\n"+
+//                        "Number of players: 2\n"+
+//                        "================================================\n", outputStreamCaptor.toString());
+//    }
 }
 
